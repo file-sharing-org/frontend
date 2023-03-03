0ac53afc3a0f640311c50cce601c64c4b164f069
Mihail <JIaIILLIa3@yandex.ru>
2023-03-03 22:51:10 +0700

    Добавил валидацию паролей и почты в регистрации

--- src/components/filePage.js
+++ src/components/filePage.js
@@ -1,10 +1,11 @@
-import {Link} from "react-router-dom";
+import {Link, Navigate} from "react-router-dom";
 import React from "react";
 import "./filePage.css";
 
 function FilePage() {
 	return (
 		<div>
+		<Navigate to="/login" />
 		<Link to="/login">34343</Link>
 			Filepage
 		</div>
--- src/components/login/registerForm.js
+++ src/components/login/registerForm.js
@@ -1,16 +1,20 @@
 import React from "react";
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import "./registerForm.css";
 import {TextField, Paper, FormControl, Button} from "@mui/material";
 import {Link} from "react-router-dom";
 
 
 function RegisterForm() {
+	const [email, setEmail] = useState('');
 	const [isValidEmail, setIsValidEmail] = useState(false);
+	const [isEmailDirty, setEmailDirty] = useState(false);
 	const [isPasswordEquals, setPasswordEquals] = useState(false);
+	const [isPasswordDity, setPasswordDirty] = useState(false);
 	const [password1, setPassword1] = useState('');
 	const [password2, setPassword2] = useState('');	
 	const checkEmail=(event)=> {
+		setEmailDirty(true);
 		if(!/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/.test(event.target.value)) {
 			// console.log("govno");
 			setIsValidEmail(false);
@@ -19,12 +23,22 @@ function RegisterForm() {
 		}
 	}
 
-	const checkPassword = (event)=>{
-		let password = event.target.value;
-		if(password==password1) {
+	const checkPassword = ()=>{
+		setPasswordDirty(true);
+		if(password2==password1) {
 			setPasswordEquals(true);
 		} else {
 			setPasswordEquals(false);
+		}
+	}
+
+	useEffect(()=>{
+		checkPassword();
+	}, [password1, password2]);
+
+	const onSubmit = ()=>{
+		if(isValidEmail && isPasswordEquals) {
+			//TODO
 		}
 	}
 	
@@ -32,19 +46,19 @@ function RegisterForm() {
 	return (
 		<Paper elevation={10} sx={{padding: '1em', 'border-radius': '10px'}}>
 			<FormControl sx={{width: '100%', '& .MuiTextField-root': { m: 1}}}>
-				{isValidEmail ? <TextField label="Почта" required onBlur={checkEmail} helperText=" "/>
-							  : <TextField label="Почта" required onBlur={checkEmail} error helperText="Неправильный формат почты"/>}
-				{isPasswordEquals?	
-					<TextField type="password" label="Пароль" onChange={(e)=>setPassword1(e.target.value)} value={password1} required />
+				{!isEmailDirty||isValidEmail ? <TextField label="Почта" required onBlur={checkEmail} onChange={(e)=>setEmail(e.target.value)} value={email} helperText=" "/>
+							  : <TextField label="Почта" required onBlur={checkEmail} onChange={(e)=>setEmail(e.target.value)} value={email} error helperText="Неправильный формат почты"/>}
+				{!isPasswordDity||isPasswordEquals?	
+					<TextField type="password" label="Пароль" onChange={(e)=>{setPassword1(e.target.value);}} value={password1} required />
 				:
-					<TextField type="password" label="Пароль" onChange={(e)=>setPassword1(e.target.value)} value={password1} required error/>
+					<TextField type="password" label="Пароль" onChange={(e)=>{setPassword1(e.target.value);}} value={password1} required error/>
 				}
-				{isPasswordEquals?
-					<TextField type="password" label="Повторите пароль" helperText=" " onChange={checkPassword} required/>	
+				{!isPasswordDity||isPasswordEquals?
+					<TextField type="password" label="Повторите пароль" helperText=" " onChange={(e)=>{setPassword2(e.target.value);}} value={password2} required/>	
 				:
-					<TextField type="password" label="Повторите пароль" helperText="Пароли не совпадают" onChange={checkPassword} required error/>
+					<TextField type="password" label="Повторите пароль" helperText="Пароли не совпадают" onChange={(e)=>{setPassword2(e.target.value);}} value={password2} required error/>
 				}
-				<Button variant="contained" sx={{m: 1}}>Зарегистрироваться</Button>
+				<Button variant="contained" sx={{m: 1}} onClick={onSubmit}>Зарегистрироваться</Button>
 			</FormControl>
 		</Paper>
 		);
