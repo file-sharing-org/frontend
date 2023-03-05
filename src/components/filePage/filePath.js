1bf6eec96ca6220f3628147837d7768fb51b3c18
Misha <misha.solodkin0126@gmail.com>
2023-03-05 20:13:44 +0700

    Перенес стили filepath в css

--- src/components/filePage/filePath.js
+++ src/components/filePage/filePath.js
@@ -2,19 +2,20 @@ import {Link, Navigate} from "react-router-dom";
 import React from "react";
 import {Container, Box} from '@mui/material';
 import {ReactComponent as HomeImg} from '../../home.svg';
+import './filePath.css';
 
 function FileElement({color='#cccccc', src=false, text=''}) {
 	if(src) {
 		console.log("ava");
 	}
 	return (
-		<Link style={{display:'flex', marginRight: '5px', textDecoration: 'none',color:'#2C3E50', fontSize:'25px'}}>
-			{src&&<div style={{width:'10px', lineHeight: '50px',backgroundColor: color}}/>}
-			{src? <HomeImg style={{backgroundColor: color, height: '100%'}}/>:
-			<div style={{lineHeight: '50px',backgroundColor: color}}>{text}</div>}
-			<div style={{position: 'relative', overflow: 'hidden', 'paddingRight': '5px'}}>
-				<div style={{position:'relative', top:-4, left:'2px', width: '0', height: '0', borderTop: '29px solid transparent', borderLeft: `30px solid black`, borderBottom: '29px solid transparent'}}/>
-				<div style={{position:'absolute', top:-4, width: '0', height: '0', borderTop: '29px solid transparent', borderLeft: `30px solid ${color}`, borderBottom: '29px solid transparent'}}/>
+		<Link className='filepath filepath_cyan'>
+			{src&&<div className="filepath__svgdiv" style={{backgroundColor: color}}/>}
+			{src? <HomeImg className='filepath__svg' style={{backgroundColor: color}}/>:
+			<div className="filepath__text" style={{backgroundColor: color}}>{text}</div>}
+			<div className='filepath__corner'>
+				<div className='corner__div corner_first'/>
+				<div className='corner__div corner_second' style={{borderLeft: `30px solid ${color}`}}/>
 			</div>
 		</Link>
 	);
@@ -22,7 +23,7 @@ function FileElement({color='#cccccc', src=false, text=''}) {
 
 function FilePath(props) {
 	return (
-		<Box sx={{bgcolor: '#cccccc', height:'50px', display: 'flex'}}>
+		<Box className="filebox filebox_white">
 			<FileElement color='#00bdd3' src={true}/>
 			<FileElement text='aboba'/>
 			<FileElement text='biba'/>
