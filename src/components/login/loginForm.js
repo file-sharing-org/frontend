101c63b2a9a4f0bba18f4984e4642c887532b323
Misha <misha.solodkin0126@gmail.com>
2023-03-05 23:16:16 +0700

    Переделал формы авторизации

--- src/components/login/loginForm.js
+++ src/components/login/loginForm.js
@@ -1,20 +1,39 @@
 import React from "react";
 import "./loginForm.css";
-import {TextField, Paper, FormControl, Button} from "@mui/material";
+import {TextField, Paper, FormControl, Button, FormHelperText} from "@mui/material";
 import {Link} from "react-router-dom";
-
+import { useForm } from "react-hook-form";
+import {useState, useEffect} from "react";
 
 function LoginForm() {
+
+	const [formValues, setFormValues] = useState({username: '', password:''});
+	const [error, setError] = useState(false);
+
+	const handleInputChange = (e) => {
+		setFormValues({
+			...formValues, [e.target.name]: e.target.value
+		});
+	}
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log(formValues);
+	}
+
 	return (
 		<Paper elevation={10} sx={{padding: '1em', 'border-radius': '10px'}}>
-			<FormControl sx={{width: '100%', '& .MuiTextField-root': { m: 1}}}>
-				<TextField label="Логин"/>
-				<TextField type="password" label="Пароль"/>
-				<Button variant="contained" sx={{m: 1}}>Войти</Button>
-				<div className="formlogin__label">
-					Нет аккаунта? <Link to="/register">Зарегистрируйтесь</Link>
-				</div>
-			</FormControl>
+			<form onSubmit={handleSubmit}>
+				<FormControl sx={{width: '100%', '& .MuiTextField-root': { m: 1}}}>
+					<TextField id='username' name='username' label="Логин" onChange={handleInputChange} value={formValues.username}/>
+					<TextField name='password' type="password" label="Пароль" onChange={handleInputChange} value={formValues.password}/>
+					<FormHelperText error>{error?'Неверные данные':' '}</FormHelperText>
+					<Button variant="contained" sx={{m: 1}} type='submit'>Войти</Button>
+					<div className="formlogin__label">
+						Нет аккаунта? <Link to="/register">Зарегистрируйтесь</Link>
+					</div>
+				</FormControl>
+			</form>
 		</Paper>
 		);
 }
--- src/components/login/registerForm.js
+++ src/components/login/registerForm.js
@@ -3,19 +3,22 @@ import {useState, useEffect} from "react";
 import "./registerForm.css";
 import {TextField, Paper, FormControl, Button} from "@mui/material";
 import {Link} from "react-router-dom";
+import { useForm, Controller} from "react-hook-form";
+
+
 
 
 function RegisterForm() {
-	const [email, setEmail] = useState('');
 	const [isValidEmail, setIsValidEmail] = useState(false);
 	const [isEmailDirty, setEmailDirty] = useState(false);
 	const [isPasswordEquals, setPasswordEquals] = useState(false);
 	const [isPasswordDity, setPasswordDirty] = useState(false);
-	const [password1, setPassword1] = useState('');
-	const [password2, setPassword2] = useState('');	
+
+
+	const [formValues, setFormValues] = useState({email:'', password1:'', password2:''});
 	const checkEmail=()=> {
 		
-		if(!/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/.test(email)) {
+		if(!/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/.test(formValues.email)) {
 			// console.log("govno");
 			setIsValidEmail(false);
 		} else {
@@ -24,10 +27,10 @@ function RegisterForm() {
 	}
 	useEffect(()=>{
 		checkEmail();
-	},[email]);
+	},[formValues.email]);
 
 	const checkPassword = ()=>{
-		if(password1.length>0 && password2.length>0 && password2==password1) {
+		if(formValues.password1.length>0 && formValues.password2.length>0 && formValues.password2==formValues.password1) {
 			setPasswordEquals(true);
 		} else {
 			setPasswordEquals(false);
@@ -37,12 +40,20 @@ function RegisterForm() {
 	useEffect(()=>{
 		console.log('effect');
 		checkPassword();
-	}, [password1, password2]);
+	}, [formValues.password1, formValues.password2]);
 
-	const onSubmit = ()=>{
+	const handleInputChange = (e) => {
+		setFormValues({
+			...formValues, [e.target.name]: e.target.value
+		});
+	}
+
+	const onSubmit = (e)=>{
+		e.preventDefault();
 		console.log("submit");
 		setPasswordDirty(true);
 		setEmailDirty(true);
+		console.log(formValues);
 		if(isValidEmail && isPasswordEquals) {
 			//TODO
 		}
@@ -51,16 +62,19 @@ function RegisterForm() {
 
 	return (
 		<Paper elevation={10} sx={{padding: '1em', 'border-radius': '10px'}}>
-			<form onSubmit={()=>onSubmit()}>
+			<form onSubmit={onSubmit}>
 				<FormControl sx={{width: '100%', '& .MuiTextField-root': { m: 1}}}>
-					<TextField label="Почта" required onChange={(e)=>{setEmail(e.target.value);setEmailDirty(true);}} value={email} helperText={isEmailDirty&&!isValidEmail?"Неправильный формат почты":" "} error={isEmailDirty&&!isValidEmail}/>
+					<TextField name='email' id='email' label="Почта" required
+						onChange={(e)=>{handleInputChange(e);setEmailDirty(true);}} value={formValues.email}
+						helperText={isEmailDirty&&!isValidEmail?"Неправильный формат почты":" "} error={isEmailDirty&&!isValidEmail}
+						inputProps={{pattern: '[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+'}}/>
 					
-					<TextField type="password" label="Пароль" onChange={(e)=>{setPassword1(e.target.value);setPasswordDirty(true);}} value={password1} required error={isPasswordDity&&!isPasswordEquals}/>
+					<TextField name='password1' type="password" label="Пароль" onChange={(e)=>{handleInputChange(e);setPasswordDirty(true)}} value={formValues.password1} required error={isPasswordDity&&!isPasswordEquals}/>
 					
-					<TextField type="password" label="Повторите пароль" helperText={isPasswordDity&&!isPasswordEquals?"Пароли не совпадают":" "} onChange={(e)=>{setPassword2(e.target.value);}} value={password2} required error={isPasswordDity&&!isPasswordEquals}/>	
+					<TextField name='password2' type="password" label="Повторите пароль" helperText={isPasswordDity&&!isPasswordEquals?"Пароли не совпадают":" "} onChange={(e)=>{handleInputChange(e);}} value={formValues.password2} required error={isPasswordDity&&!isPasswordEquals}/>	
 					
-					<Button variant="contained" sx={{m: 1}} onClick={onSubmit}>Зарегистрироваться</Button>
-					<input type="submit" style={{display:'none'}}/>
+					<Button variant="contained" sx={{m: 1}} type="submit">Зарегистрироваться</Button>
+					
 				</FormControl>
 			</form>
 		</Paper>
