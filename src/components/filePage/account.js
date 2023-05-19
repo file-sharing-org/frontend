04a4346274497d3216f88f7f1e59de35b60e7511
Misha <misha.solodkin0126@gmail.com>
2023-05-19 12:18:00 +0700

    Удалил иконку пользователя

--- src/components/filePage/account.js
+++ src/components/filePage/account.js
@@ -4,7 +4,6 @@ import {Avatar} from '@mui/material';
 function AccountInfo(props) {
 	return (
 		<div style={{'display':'flex', height:props.height}}>
-			<Avatar src={props.src} sx={{width: props.height, height:props.height}}/>
 			<div style={{marginLeft:'10px', height: props.height, 'lineHeight': props.height, 'textAlign':'center', fontWeight: 800, fontSize: `calc(${props.height}/2)`}}>{props.text}</div>
 		</div>
 	);
